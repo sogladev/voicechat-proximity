8eee56881b806d3a0fb0996231091553cd4ab84c
Jelle Meeus <sogladev@gmail.com>
2025-02-27 16:59:33 +0100

    setup dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,4 +15,17 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: "./", // Ensures assets work when served from static folder
+  build: {
+    outDir: "../backend/static", // Moves build files to backend
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:22142', // REST API calls
+      '/ws': {
+        target: 'ws://localhost:22142', // WebSocket connections
+        ws: true,
+      },
+    },
+  },
 })
